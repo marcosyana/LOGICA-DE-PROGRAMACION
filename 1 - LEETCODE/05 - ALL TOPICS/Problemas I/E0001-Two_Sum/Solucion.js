bfde74618bc97bf75a9d6bd4e1fbb99eb12f5b26
Marcos Yana <marcosyana01@gmail>
2024-09-22 18:46:58 -0400

    Actualizacion

--- 1 - LEETCODE/05 - ALL TOPICS/Problemas I/E0001-Two_Sum/Solucion.js
+++ 1 - LEETCODE/05 - ALL TOPICS/Problemas I/E0001-Two_Sum/Solucion.js
@@ -24,3 +24,6 @@ const twoSum = function (nums, target){
 console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
 console.log(twoSum([3, 2, 4], 6)); // [0, 1]
 console.log(twoSum([3, 3], 6)); // [0, 1]
+
+
+
